fcf478d09cc10195fac68ea10beec87b32dd6fac
zzee86 <hassanzain137@gmail.com>
2023-07-20 13:24:52 +0100

    display movie and tv providers

--- movie-app-mvc/Controllers/DetailsController.cs
+++ movie-app-mvc/Controllers/DetailsController.cs
@@ -85,6 +85,7 @@ namespace movie_app_mvc.Controllers
 
 
             await GetMediaRecommendations(media_type, id, userID);
+            await GetMediaProviders(media_type, id);
 
             if (movie_tv_details != null)
             {
@@ -457,5 +458,43 @@ namespace movie_app_mvc.Controllers
                 return info;
             }
         }
+
+        private async Task<List<MediaWatchProviders.Buy>> GetMediaProviders(string media_type, int id)
+        {
+            string apiUrl = $"https://api.themoviedb.org/3/{media_type}/{id}/watch/providers?api_key=ca80dfbe1afe5a1a97e4401ff534c4e4";
+            MediaWatchProviders.Root movieInfo = await FetchMediaProviders(apiUrl);
+
+            return await GetFilteredProviders(movieInfo);
+        }
+
+        private async Task<MediaWatchProviders.Root> FetchMediaProviders(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsStringAsync();
+                var info = JsonConvert.DeserializeObject<MediaWatchProviders.Root>(json);
+
+                return info;
+            }
+        }
+        private async Task<List<MediaWatchProviders.Buy>> GetFilteredProviders(MediaWatchProviders.Root movieInfo)
+        {
+            List<MediaWatchProviders.Buy> providers = movieInfo.results.US.buy
+                .Concat(movieInfo.results.GB.buy)
+                .GroupBy(p => p.provider_id) // Group providers by provider_id to remove duplicates
+                .Select(group => group.OrderBy(p => p.display_priority).First()) // Select the providers with the lowest display_priority
+                .OrderBy(p => p.display_priority)
+                .Take(5) // Take up to 5 providers with the lowest display_priority (most important)
+                .ToList();
+
+            ViewBag.Providers = providers;
+
+            return providers;
+        }
+
+
     }
 }
---
+++ movie-app-mvc/Models/MediaWatchProviders.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace movie_app_mvc.Models
+{
+    public class MediaWatchProviders
+    {
+        public class Buy
+        {
+            public string logo_path { get; set; }
+            public int provider_id { get; set; }
+            public string provider_name { get; set; }
+            public int display_priority { get; set; }
+
+            public string logo_path_url => string.Format("https://image.tmdb.org/t/p/original{0}", logo_path);
+        }
+
+        public class GB
+        {
+            public string link { get; set; }
+            public List<Buy> buy { get; set; }
+
+            public List<Rent> rent { get; set; }
+        }
+
+        public class Rent
+        {
+            public string logo_path { get; set; }
+            public int provider_id { get; set; }
+            public string provider_name { get; set; }
+            public int display_priority { get; set; }
+        }
+
+        public class Results
+        {
+            public GB GB { get; set; }
+            public US US { get; set; }
+        }
+
+        public class Root
+        {
+            public int id { get; set; }
+            public Results results { get; set; }
+        }
+
+        public class US
+        {
+            public string link { get; set; }
+            public List<Rent> rent { get; set; }
+            public List<Buy> buy { get; set; }
+        }
+    }
+}
+
